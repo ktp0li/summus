69511c688faab82ba67ac3dc7c266ce7ce6ffaed
Beshenka <mambatamba6@gmail.com>
2023-05-15 20:12:07 +0300

    better vpc

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,3 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 def add_exit_button(builder: InlineKeyboardBuilder):
     builder.button(text='В главное меню', callback_data='exit')
-
-def get_endpoint() -> str:
-    return 'https://vpc.ru-moscow-1.hc.sbercloud.ru'
