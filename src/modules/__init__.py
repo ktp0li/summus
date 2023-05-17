f86013e1f7cf868a6ffe67ddd49733a2cfc5b88c
Beshenka <mambatamba6@gmail.com>
2023-05-17 15:26:56 +0300

    add monitoring

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -2,5 +2,6 @@ from .vpc import VPC
 from .eps import EPS
 from .subnet import SUBNET
 from .nat import NAT
+from .ces import CES
 
-modules = (EPS, VPC, SUBNET, NAT)
+modules = (EPS, VPC, SUBNET, NAT, CES)
